dfc229c9e37f67a5b91b0ae32218278481306dac
agaur12 <102323862+agaur12@users.noreply.github.com>
2023-09-17 00:13:04 -0500

    Added mapping feature

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from MapEmbed import map_inputs
+
+api_key = 'AIzaSyA6cXymaX959J3CYjXTcNhCTBFTt9qi6pM'
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return render_template('index.html')
-
+def embed():
+    start, end, mode, path_string, markers = map_inputs("5900 N Keating Ave", "5501 N Kedzie Ave", "driving")
+    return render_template('embed.html', start=start, end=end, mode=mode, path=path_string, markers=markers, api_key=api_key)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
