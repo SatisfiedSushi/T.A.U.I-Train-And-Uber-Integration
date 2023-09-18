c5bfd19174a6277106a54ace970d63622afc1903
agaur12 <102323862+agaur12@users.noreply.github.com>
2023-09-17 21:17:39 -0500

    fixed website error that was caused by requests.

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template, request
 from MapEmbed import map_inputs
-import UberDeepLink
+from UberDeepLink import addy_to_lat_long
 
 api_key = 'AIzaSyA6cXymaX959J3CYjXTcNhCTBFTt9qi6pM'
 
 app = Flask(__name__)
 
-@app.route('/handle_post', methods=['POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def handle_post():
     if request.method == 'POST':
         start = request.form['start']
         end = request.form['end']
-        start_lat, start_long, end_lat, end_long, origin, destination = UberDeepLink.addy_to_lat_long(start, end)
-        url = f"https://m.uber.com/ul/?client_id=<CLIENT_ID>&action=setPickup&pickup[latitude]={start_lat}&pickup[longitude]={start_long}&pickup[formatted_address]={origin}&dropoff[latitude]={end_lat}&dropoff[longitude]={end_long}&dropoff[formatted_address]={destination}"
-        return render_template('input.html', start=start, end=end, api_key=api_key), url
-
+        start_lat, start_long, end_lat, end_long = addy_to_lat_long(start, end, api_key)
+        return render_template('input.html', start=start, end=end, api_key=api_key, start_lat=start_lat, start_long=start_long, end_lat=end_lat, end_long=end_long)
+    return render_template('input.html', start='', end='', api_key=api_key)
 
 if __name__ == '__main__':
     app.run(debug=True)
