71d24c3345385b61138ccdb476157f359e0dff03
agaur12 <102323862+agaur12@users.noreply.github.com>
2023-09-19 23:36:35 -0500

    tested json handling

--- app.py
+++ app.py
@@ -2,14 +2,13 @@ from flask import Flask, Response, render_template, request, send_from_directory
 import json
 from MapEmbed import map_inputs
 from UberDeepLink import addy_to_lat_long
-from GetLocation import get_current_location
 
 api_key = 'AIzaSyA6cXymaX959J3CYjXTcNhCTBFTt9qi6pM'
 
 app = Flask(__name__)
 
-
-@app.route('/' """, methods=['GET', 'POST']""")
+"""
+@app.route('/' , methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         start = request.form['start']
@@ -24,6 +23,17 @@ def index():
 def receive_location_data():
     #Code to receive json data
     None
+"""
+@app.route('/', methods=['GET'])
+def send_js():
+    json_file = open('data.json')
+    data = json.load(json_file)
+    name1 = data['employees'][0]['firstName']
+    name2 = data['employees'][1]['firstName']
+    name3 = data['employees'][2]['firstName']
+    return render_template('index.html', name1=name1, name2=name2, name3=name3)
+
+
 
 
 if __name__ == '__main__':
