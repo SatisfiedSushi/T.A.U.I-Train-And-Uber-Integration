d443edd21aef4216bfd3e835a521acf711588e2f
agaur12 <102323862+agaur12@users.noreply.github.com>
2023-09-20 01:19:54 -0500

    added feature to get current location and set to start location.

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask, Response, render_template, request, send_from_directory
 import json
 from MapEmbed import map_inputs
 from UberDeepLink import addy_to_lat_long
+from GoogleMapsAPIs import *
 
 api_key = 'AIzaSyA6cXymaX959J3CYjXTcNhCTBFTt9qi6pM'
 
 app = Flask(__name__)
 
-"""
+
 @app.route('/' , methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -18,22 +19,14 @@ def index():
     return render_template('input.html', start='', end='', api_key=api_key)
 
 
-
-@app.route('/')
+@app.route('/receive_location_data', methods=['GET'])
 def receive_location_data():
-    #Code to receive json data
-    None
-"""
-@app.route('/', methods=['GET'])
-def send_js():
-    json_file = open('data.json')
-    data = json.load(json_file)
-    name1 = data['employees'][0]['firstName']
-    name2 = data['employees'][1]['firstName']
-    name3 = data['employees'][2]['firstName']
-    return render_template('index.html', name1=name1, name2=name2, name3=name3)
-
-
+    lat = request.args.get('latitude')
+    long = request.args.get('longitude')
+    accuracy = request.args.get('accuracy')
+    print(lat, long, accuracy)
+    address = get_address_from_lat_lng(lat, long, api_key)
+    return address
 
 
 if __name__ == '__main__':
