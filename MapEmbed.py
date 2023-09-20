df950fe6b32e37d5c4e742dd64b66b2fa3715a2b
agaur12 <102323862+agaur12@users.noreply.github.com>
2023-09-20 06:30:19 -0500

    added basic functionality between frontend and backend

--- MapEmbed.py
+++ MapEmbed.py
@@ -1,5 +1,4 @@
 import googlemaps
-import polyline
 
 
 # Replace with your own API key
@@ -8,38 +7,11 @@ api_key = 'AIzaSyA6cXymaX959J3CYjXTcNhCTBFTt9qi6pM'
 # Initialize the Google Maps client
 gmaps = googlemaps.Client(api_key)
 
-# Function to decode polyline points
-def decode_polyline(polyline_str):
-    return polyline.decode(polyline_str)
-def map_inputs(start, end):
-    start, end = start, end
+def map_inputs(lat, long):
+    # Generate markers for location
+    label = 'P'
+    marker_color = 'green'
+    marker = f'markers=color:{marker_color}|label:{label}|{lat},{long}&'
 
-    # Get directions
-    directions = gmaps.directions(start, end)
-
-    # Extract the path from the directions response
-    path = []
-    for step in directions[0]['legs'][0]['steps']:
-        path.extend(decode_polyline(step['polyline']['points']))
-
-    # Generate the path as a string for the static map
-    path_color = '0x0000ff'
-    path_weight = 5
-    path_string = f'color:{path_color}|weight:{path_weight}|'
-    path_string += '|'.join([f"{lat},{lng}" for lat, lng in path])
-
-
-    # Get coordinates of start and end locations
-    start_location = directions[0]['legs'][0]['start_location']
-    end_location = directions[0]['legs'][0]['end_location']
-
-    # Generate markers for start and end locations
-    start_label = 'S'
-    end_label = 'E'
-    start_marker_color = 'green'
-    end_marker_color = 'red'
-    markers = f'markers=color:{start_marker_color}|label:{start_label}|{start_location["lat"]},{start_location["lng"]}&'
-    markers += f'markers=color:{end_marker_color}|label:{end_label}|{end_location["lat"]},{end_location["lng"]}'
-
-    return start, end, path_string, markers
+    return marker
 
--- app.py
+++ app.py
@@ -1,22 +1,60 @@
 from flask import Flask, Response, render_template, request, send_from_directory
 import json
+
 from MapEmbed import map_inputs
-from GoogleMapsAPIs import *
+from GetDirections import *
+
+# from GoogleMapsAPIs import *
 
 api_key = 'AIzaSyA6cXymaX959J3CYjXTcNhCTBFTt9qi6pM'
 
 app = Flask(__name__)
 
 
-@app.route('/' , methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         start = request.form['start']
         end = request.form['end']
-        print(start, end)
-        start_lat, start_long = get_lat_lng_from_address(start, api_key)
-        end_lat, end_long = get_lat_lng_from_address(end, api_key)
-        return render_template('index.html', start=start, end=end, api_key=api_key, start_lat=start_lat, start_long=start_long, end_lat=end_lat, end_long=end_long)
+        # Get the route data
+        route_data = get_chosen_route_data(start, end)
+        # Extract individual parts into variables
+        mode = route_data["mode"]
+        distance = route_data["distance"]
+        time = route_data["time"]
+        departure_time = route_data["departure_time"]
+        arrival_time = route_data["arrival_time"]
+
+        # Get path of route
+        directions = route_data["directions"]
+        path = extract_polyline(directions)
+
+        # Get locations and markers for each location
+        stop_lat = []
+        stop_long = []
+        markers = []
+        for stop in route_data["transit_stops"]:
+            stop_lat.append(stop['location']['lat'])
+            stop_long.append(stop['location']['lng'])
+            markers.append(map_inputs(stop['location']['lat'], stop['location']['lng']))
+        start_location = directions[0]['legs'][0]['start_location']
+        end_location = directions[0]['legs'][0]['end_location']
+        end_lat, end_long = end_location['lat'], end_location['lng']
+        start_lat, start_long = start_location['lat'], start_location['lng']
+        markers.append(map_inputs(start_lat, start_long))
+        markers.append(map_inputs(end_lat,end_long))
+
+        # Get first and last station lat and long
+        stop1_lat = stop_lat[0]
+        stop1_long = stop_long[0]
+        stop1_address = get_address_from_lat_lng(stop1_lat, stop1_long, api_key)
+        stop2_lat = stop_lat[-1]
+        stop2_long = stop_long[-1]
+        stop2_address = get_address_from_lat_lng(stop2_lat, stop2_long, api_key)
+        return render_template('index.html', start=start, end=end, api_key=api_key, markers=markers, mode=mode,
+                               distance=distance, time=time, departure_time=departure_time, arrival_time=arrival_time,
+                               start_lat=start_lat, start_long=start_long, end_lat=end_lat, end_long=end_long,
+                               stop1_lat=stop1_lat, stop1_long=stop1_long, stop1_address=stop1_address, stop2_lat=stop2_lat, stop2_long=stop2_long, stop2_address=stop2_address, path=path)
     return render_template('index.html', start='', end='', api_key=api_key)
 
 
