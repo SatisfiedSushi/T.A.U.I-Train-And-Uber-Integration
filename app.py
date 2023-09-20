6adf105902af70f337bd70d2f0ff3c4fd2348819
agaur12 <102323862+agaur12@users.noreply.github.com>
2023-09-20 02:14:53 -0500

    fixed some bugs and cleaned up code

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, Response, render_template, request, send_from_directory
 import json
 from MapEmbed import map_inputs
-from UberDeepLink import addy_to_lat_long
 from GoogleMapsAPIs import *
 
 api_key = 'AIzaSyA6cXymaX959J3CYjXTcNhCTBFTt9qi6pM'
@@ -14,17 +13,17 @@ def index():
     if request.method == 'POST':
         start = request.form['start']
         end = request.form['end']
-        start_lat, start_long, end_lat, end_long = addy_to_lat_long(start, end, api_key)
-        return render_template('input.html', start=start, end=end, api_key=api_key, start_lat=start_lat, start_long=start_long, end_lat=end_lat, end_long=end_long)
-    return render_template('input.html', start='', end='', api_key=api_key)
+        print(start, end)
+        start_lat, start_long = get_lat_lng_from_address(start, api_key)
+        end_lat, end_long = get_lat_lng_from_address(end, api_key)
+        return render_template('index.html', start=start, end=end, api_key=api_key, start_lat=start_lat, start_long=start_long, end_lat=end_lat, end_long=end_long)
+    return render_template('index.html', start='', end='', api_key=api_key)
 
 
 @app.route('/receive_location_data', methods=['GET'])
 def receive_location_data():
     lat = request.args.get('latitude')
     long = request.args.get('longitude')
-    accuracy = request.args.get('accuracy')
-    print(lat, long, accuracy)
     address = get_address_from_lat_lng(lat, long, api_key)
     return address
 
